e4736c827759cadf3fb88c1262f5ded0b88d8411
Piotr Olech <piotr.olech@kissdigital.com>
2020-06-08 07:29:47 +0200

    Release v1.4.23

---
+++ dotpay_mobile_sdk_ios_1.4.23/example/Demo App/Pods/DotPaySDK/dotpay_mobile_sdk_ios_1.0.3/lib/DotPaySDK.framework/Headers/DPCollectionViewLayout.h
@@ -0,0 +1,13 @@
+//
+//  DPCollectionViewLayout.h
+//  DotPaySDK
+//
+//  Created by Mateusz Maćkowiak on 07/05/15.
+//  Copyright (c) 2015 Dotpay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DPCollectionViewLayout : UICollectionViewFlowLayout
+
+@end
---
+++ dotpay_mobile_sdk_ios_1.4.23/example/Demo App/Pods/DotPaySDK/dotpay_mobile_sdk_ios_1.0.3/lib/DotPaySDK.framework/Headers/DPPaymentHistory.h
@@ -0,0 +1,121 @@
+//
+//  DPPaymentHistory.h
+//  DotPaySDK
+//
+//  Created by Rafał Prążyński on 25/03/15.
+//  Copyright (c) 2015 Dotpay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class DPPaymentHistory;
+@class DPPaymentSummary;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol DPPaymentHistoryObserver <NSObject>
+
+@optional
+
+- (void)willUpdatePaymentSummariesForPaymentHistory:(DPPaymentHistory *)paymentHistory;
+
+- (void)didUpdatePaymentSummariesForPaymentHistory:(DPPaymentHistory *)paymentHistory;
+
+- (void)paymentHistory:(DPPaymentHistory *)paymentHistory willRemovePaymentSummary:(DPPaymentSummary *)paymentSummary;
+
+- (void)paymentHistory:(DPPaymentHistory *)paymentHistory didRemovePaymentSummary:(DPPaymentSummary *)paymentSummary;
+
+- (void)paymentHistory:(DPPaymentHistory *)paymentHistory willAddPaymentSummary:(DPPaymentSummary *)paymentSummary;
+
+- (void)paymentHistory:(DPPaymentHistory *)paymentHistory didAddPaymentSummary:(DPPaymentSummary *)paymentSummary;
+
+- (void)paymentHistory:(DPPaymentHistory *)paymentHistory didUpdatePaymentSummary:(DPPaymentSummary *)paymentSummary;
+
+- (void)paymentHistory:(DPPaymentHistory *)paymentHistory didFailedToUpdatePaymentSummary:(DPPaymentSummary *)paymentSummary withError:(NSError *)error;
+
+@end
+
+@interface DPPaymentHistory : NSObject
+
+/*!
+ *  Updates details for single payment summary
+ */
+- (void)refreshPaymentSummaryDetails:(DPPaymentSummary*)paymentSummary withCompletion:(void (^)(BOOL success))completion;
+
+@property (nonatomic, strong, readonly) NSString *filePath;
+
+/*!
+ *  @return Singleton instance of DPPaymentHistory object.
+ */
++ (instancetype)sharedInstance;
+
+/*!
+ *  @param filePath path at which history will be saved
+ *  capacity will be set to default value (kDPPaymentDefaultHistoryCapacity)
+ */
+- (instancetype)initWithFilePath:(NSString *)filePath;
+
+/*!
+ *  History of payments
+ *
+ *  @param filePath path at which history will be saved
+ *  @param capacity maximum number of elements in history
+ *
+ */
+- (instancetype)initWithFilePath:(NSString *)filePath capacity:(NSUInteger)capacity NS_DESIGNATED_INITIALIZER;
+
+/*!
+ *  maximum number of elements in history
+ */
+@property (nonatomic, assign, readonly) NSUInteger capacity;
+
+/*!
+ *  full payment history
+ *
+ *  @return NSArray that contains history
+ */
+@property (nonatomic, readonly) NSArray *paymentHistory;
+
+/*!
+ *  Use this method to save history
+ */
+- (void)save;
+
+/*!
+ *  Use this method to add elements to history
+ *
+ *  @param payment DPPaymentSummary object
+ */
+- (void)addPaymentSummary:(DPPaymentSummary *)paymentSummary;
+
+/*!
+ *  Use this method to remove elements to history
+ *
+ *  @param payment DPPaymentSummary object
+ */
+- (void)removePaymentSummary:(DPPaymentSummary *)paymentSummary;
+
+/*!
+ *  Use this method to clean all history
+ */
+- (void)clear;
+
+
+- (void)update;
+
+- (void)updatePaymentSummary:(DPPaymentSummary *)paymentSummary;
+
+@end
+
+
+@interface DPPaymentHistory (Observers)
+
+@property (nonatomic, readonly) NSArray *observers;
+
+- (void)registerObserver:(id <DPPaymentHistoryObserver>)observer;
+
+- (void)unRegisterObserver:(id <DPPaymentHistoryObserver>)observer;
+
+@end
+
+NS_ASSUME_NONNULL_END
