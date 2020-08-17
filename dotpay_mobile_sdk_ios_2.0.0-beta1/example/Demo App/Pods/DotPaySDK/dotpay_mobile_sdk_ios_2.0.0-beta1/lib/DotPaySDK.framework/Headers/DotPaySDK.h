67416676d6d9fe909e1be5088a0f47b4d6c57987
Piotr Olech <piotr.olech@kissdigital.com>
2020-08-17 12:06:41 +0200

    Update demo app

---
+++ dotpay_mobile_sdk_ios_2.0.0-beta1/example/Demo App/Pods/DotPaySDK/dotpay_mobile_sdk_ios_2.0.0-beta1/lib/DotPaySDK.framework/Headers/DotPaySDK.h
@@ -0,0 +1,45 @@
+//
+//  DotPaySDK.h
+//  DotPaySDK
+//
+//  Created by Mariusz Śpiewak on 06.03.2015.
+//  Copyright (c) 2015 Dotpay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <DotPaySDK/DotPay.h>
+
+#import <DotPaySDK/DPPaymentChannel.h>
+#import <DotPaySDK/DPPaymentSummary.h>
+#import <DotPaySDK/DPCurrency.h>
+#import <DotPaySDK/DPPaymentInfo.h>
+#import <DotPaySDK/DPPaymentDetails.h>
+
+#import <DotPaySDK/DPOneClickManager.h>
+#import <DotPaySDK/DPFavoritePaymentMethod.h>
+#import <DotPaySDK/DPOneClickPaymentCard.h>
+#import <DotPaySDK/DPOneClickPaymentCardInfo.h>
+#import <DotPaySDK/DPOneClickPaymentCardBrand.h>
+
+//
+#import <DotPaySDK/DPPaymentHistory.h>
+#import <DotPaySDK/DPOneClickExternalPaymentCard.h>
+#import <DotPaySDK/DPOneClickCardRegistrationData.h>
+
+//Validation
+#import <DotPaySDK/DPPaymentInfoValidator.h>
+
+//Controllers
+#import <DotPaySDK/DPDotPayViewController.h>
+
+#import <DotPaySDK/DPPaymentHistoryTableViewController.h>
+#import <DotPaySDK/DPPaymentHistoryViewController.h>
+
+#import <DotPaySDK/DPPaymentChannelsCollectionViewController.h>
+#import <DotPaySDK/DPPaymentChannelsViewController.h>
+
+#import <DotPaySDK/DPPaymentChannelSelectionViewControllerDelegate.h>
+#import <DotPaySDK/DPOneClickPaymentCardManagerViewController.h>
+#import <DotPaySDK/DPOneClickPaymentCardManagerTableViewController.h>
+#import <DotPaySDK/DPNewOneClickPaymentCardViewController.h>
